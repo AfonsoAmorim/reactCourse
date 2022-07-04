27d2673edf8ccff092a581dfa907300e23c69521
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-07-04 18:00:56 -0300

    utilizando state sem erros

--- section-3/src/componentes/Lista.js
+++ section-3/src/componentes/Lista.js
@@ -4,13 +4,27 @@ import {useState} from "react";
 const Lista = () => {
     const [lista] = useState(["Aline", "Carlos", "Amorim", "Maria"]);
  
+    const [users] = useState([
+        {id: 1, name: "Aline Josi", age: 22},
+        {id: 2, name: "ALonso Morais", age: 12},
+        {id: 3, name: "Jonas Seven", age: 55},
+        {id: 4, name: "Livia Aline", age: 77},
+    ])
+
     return (
     <div>
         <ul>
-            {lista.map((i)=>(
-                <li>{i.toUpperCase()}</li>
+            {lista.map((item, indice)=>(
+                <li key={indice}>{item.toUpperCase()}</li>
             ))}
         </ul>
+        <ul>
+            {users.map((usuario)=>(
+              <li key={usuario.id}> {usuario.name} - {usuario.age}</li>  
+            ))}
+        </ul>
+
+
     </div>
   ) 
 ;}
